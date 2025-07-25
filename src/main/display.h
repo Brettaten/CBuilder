d399f27d0098e4879d5a891c22c5fcfa4ce3329c
Oskar Richter <netmail678@gmail.com>
2025-07-25 16:13:45 +0200

    add macros for printing on the console

--- src/main/display.h
+++ src/main/display.h
@@ -4,5 +4,8 @@
 #define SEPERATOR "%s\n", "----------------------------------------------------------------------------------------------------"
 #define HEADING "%s\n"
 #define LINE "\t%-30.30s\t%s\n"
+#define INFO "%s\n"
+#define LIST "-\t%s\n"
+#define EMPTY "\n"
 
 #endif
