84f3fa9fc45cd1048a426ee8649fab7a1e57d9e3
Oskar Richter <netmail678@gmail.com>
2025-08-02 16:22:50 +0200

    add directory functions

--- src/main/c/Directory/directory.c
+++ src/main/c/Directory/directory.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <time.h>
 
 #include "directory.h"
@@ -5,19 +6,19 @@
 
 typedef struct Directory
 {
-    char *path;
-    char *name;
+    char path[MAX_LENGTH_PATH];
+    char name[MAX_LENGTH_NAME];
 
     int entryAmount;
-    Entry *entries;
+    Entry **entries;
 } Directory;
 
 typedef struct Entry
 {
-    char *path;
-    char *name;
-    char *type;
-    time_t *lastModified;
+    char path[MAX_LENGTH_PATH];
+    char name[MAX_LENGTH_NAME];
+    int type;
+    time_t lastModified;
 } Entry;
 
 #ifdef WIN
@@ -83,3 +84,107 @@ Directory *directoryGet(char *path)
 }
 
 #endif
+
+char *directoryGetPath(Directory *dir)
+{
+    if(dir == NULL){
+        printf("[ERROR] : Directory is null | directoryGetPath");
+        return NULL;
+    }
+
+    return dir->path;
+}
+
+char *directoryGetName(Directory *dir)
+{
+    if(dir == NULL){
+        printf("[ERROR] : Directory is null | directoryGetName");
+        return NULL;
+    }
+
+    return dir->name;
+}
+
+int directoryGetEntryAmount(Directory *dir)
+{
+    if(dir == NULL){
+        printf("[ERROR] : Directory is null | directoryGetEntryAmount");
+        return -1;
+    }
+
+    return dir->entryAmount;
+}
+
+Entry *directoryGetSubDir(Directory *dir, char *name)
+{
+    return nullptr;
+}
+
+Entry *directoryGetFile(Directory *dir, char *name)
+{
+    return nullptr;
+}
+
+Directory *directoryGetParent(Directory *dir)
+{
+    return nullptr;
+}
+
+Directory *directoryGetSub(Directory *dir, char *name)
+{
+    return nullptr;
+}
+
+void directoryFree(Directory *dir)
+{
+    for(int i = 0; i < dir->entryAmount; i++){
+        entryFree(dir->entries[i]);
+    }
+    free(dir->entries);
+    free(dir);
+}
+
+char *entryGetPath(Entry *entry)
+{
+    if(entry == NULL){
+        printf("[ERROR] : Entry is null | entryGetPath");
+        return NULL;
+    }
+
+    return entry->path;
+}
+
+char *entryGetName(Entry *entry)
+{
+    if(entry == NULL){
+        printf("[ERROR] : Entry is null | entryGetName");
+        return NULL;
+    }
+
+    return entry->name;
+}
+
+int entryGetType(Entry *entry)
+{
+    if(entry == NULL){
+        printf("[ERROR] : Entry is null | entryGetType");
+        return -1;
+    }
+
+    return entry->type;
+}
+
+time_t *entryGetLastModified(Entry *entry)
+{
+    if(entry == NULL){
+        printf("[ERROR] : Entry is null | entryGetLasModified");
+        return NULL;
+    }
+
+    return entry->lastModified;
+}
+
+void *entryFree(Entry *entry)
+{
+    free(entry);
+}
--- src/main/c/Directory/directory.h
+++ src/main/c/Directory/directory.h
@@ -10,7 +10,146 @@ typedef struct Directory Directory;
  */
 typedef struct Entry Entry;
 
+#define FILE 1
+#define DIRECTORY 2
+
+#define MAX_LENGTH_NAME 512
+#define MAX_LENGTH_PATH 4096
+
+/**
+ * Function used to get the directory at the specified path
+ * 
+ * @param path The path to the directory
+ * 
+ * @return Success: A directory struct of this path | Failure: null
+ * 
+ * @note The returned directory has to be freed by the caller
+ */
 Directory *directoryGet(char *path);
+
+/**
+ * Function used to get the path of a directory 
+ * 
+ * @param dir The directory
+ * 
+ * @return Success: the path to the directory | Failure: null
+ */
+char *directoryGetPath(Directory *dir);
+
+/**
+ * Function used to get the name of a directory
+ * 
+ * @param dir The directory
+ * 
+ * @return Success: the name of the directory | Failure: null
+ */
+char *directoryGetName(Directory *dir);
+
+/**
+ * Function used to get the amount of entries in a directory
+ * 
+ * @param dir The directory
+ * 
+ * @return Success: the amount of entries in the directory | Failure: -1
+ */
+int directoryGetEntryAmount(Directory *dir);
+
+/**
+ * Function used to get a sub directory with the passed name
+ * 
+ * @param dir The directory
+ * @param name The name of the potential sub directory
+ * 
+ * @return Success: A pointer to an entry struct if a sub directory was found | Failure: null
+ * 
+ * @note The returned entry is a deep copy and thus has to be freed by the caller
+ */
+Entry *directoryGetSubDir(Directory *dir, char *name);
+
+/**
+ * Function used to get a file with the passed name
+ * 
+ * @param dir The directory
+ * @param name The name of the potential file
+ * 
+ * @return Success: A pointer to an entry struct if a file was found | Failure: null
+ * 
+ * @note The returned entry is a deep copy and thus has to be freed by the caller
+ */
+Entry *directoryGetFile(Directory *dir, char *name);
+
+/**
+ * Function used to get the parent directory of the passed directory
+ * 
+ * @param dir The directory
+ * 
+ * @return Success: The parent directory if one exists | Failure: null
+ * 
+ * @note The returned directory has to be freed by the caller
+ */
+Directory *directoryGetParent(Directory *dir);
+
+/**
+ * Function used to get the sub directory with the specified name of the passed directory
+ * 
+ * @param dir The directory
+ * @param name The name of the sub directory
+ * 
+ * @return Success: The sub directory if it exists | Failure: null
+ * 
+ * @note The returned directory has to be freed by the caller
+ */
+Directory *directoryGetSub(Directory *dir, char *name);
+
+/**
+ * Function used to free a directory
+ * 
+ * @param dir The directory
+ */
+void directoryFree(Directory *dir);
+
+/**
+ * Function used to get the path of an entry
+ * 
+ * @param entry The entry
+ * 
+ * @return Success: the path to the entry | Failure: null
+ */
+char *entryGetPath(Entry *entry);
+
+/**
+ * Function used to get the name of an entry
+ * 
+ * @param entry The entry
+ * 
+ * @return Success: then name of the entry | Failure: null
+ */
+char *entryGetName(Entry *entry);
+
+/**
+ * Function used to get the type of an entry
+ * 
+ * @param entry The entry
+ * 
+ * @return Success: A number that represents the entry type. Use the specified macros | Failure: -1
+ */
+int entryGetType(Entry *entry);
+
+/**
+ * Function used to get the time, where the entry was last modified
+ * 
+ * @param entry The entry
+ * 
+ * @return Success: The time, where the entry was last modified | Failure: null
+ */
+time_t *entryGetLastModified(Entry *entry);
+
+/**
+ * Function used to free an entry
+ * 
+ * @param entry The entry
+ */
+void *entryFree(Entry *entry);
 
 #endif
 
