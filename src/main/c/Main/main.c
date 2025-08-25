45f210f41921be1a10f181bdc2b9f54db62d8a56
Oskar Richter <netmail678@gmail.com>
2025-08-25 15:07:45 +0200

    add function splitFile

--- src/main/c/Main/main.c
+++ src/main/c/Main/main.c
@@ -911,9 +911,9 @@ void testBuild(char *path)
             for (int i = 0; i < directoryGetEntryAmount(tempDirSrc); i++)
             {
 
-                Entry *entry = directoryGetEntryAt(tempDirSrc, i);
+                Entry *entrySrc = directoryGetEntryAt(tempDirSrc, i);
 
-                if (entry == NULL)
+                if (entrySrc == NULL)
                 {
                     printf("[ERROR] : function directoryGetEntryAt failed | testBuild \n");
                     return;
@@ -922,7 +922,7 @@ void testBuild(char *path)
                 if (entryGetType == TYPE_DIRECTORY)
                 {
 
-                    Directory *newSrc = directoryGet(entryGetPath(entry));
+                    Directory *newSrc = directoryGet(entryGetPath(entrySrc));
 
                     if (newSrc == NULL)
                     {
@@ -930,7 +930,7 @@ void testBuild(char *path)
                         return;
                     }
 
-                    bool st1 = directoryCreate(directoryGetPath(tempDirDest), entryGetName(entry));
+                    bool st1 = directoryCreate(directoryGetPath(tempDirDest), entryGetName(entrySrc));
 
                     if (st1 == false)
                     {
@@ -941,7 +941,7 @@ void testBuild(char *path)
                     char temp[MAX_LENGTH_PATH];
                     strcpy(temp, directoryGetPath(tempDirDest));
                     strcat(temp, "/");
-                    strcat(temp, entryGetName(entry));
+                    strcat(temp, entryGetName(entrySrc));
 
                     Directory *newDest = directoryGet(temp);
 
@@ -961,21 +961,65 @@ void testBuild(char *path)
                 else
                 {
 
-                    String *extension = utilGetEx(entryGetName(entry));
-                    String *name = utilGetEx(entryGetName(entry));
+                    String *extension = utilGetEx(entryGetName(entrySrc));
+                    String *name = utilGetEx(entryGetName(entrySrc));
+
+                    Entry *entryDest = directoryGetEntry(directoryGetPath(tempDirDest), entryGetName(entrySrc), TYPE_FILE);
 
                     if (stringEquals(extension, cExt))
                     {
+                        int delete = 2;
+                        for (int i = 0; i < directoryGetEntryAmount(tempDirDest); i++)
+                        {
+                            Entry *tempEntryDest = directoryGetEntryAt(tempDirDest, i);
+                            String *tempName = stringCreate(entryGetName(tempEntryDest));
+                            String *subName = stringSub(name, 0, stringLength(name) - 1);
+
+                            if (stringEquals(subName, name))
+                            {
+                                if (delete == 0)
+                                {
+                                    remove(entryGetPath(tempEntryDest));
+                                }
+                                else if (entryGetLastModified(tempEntryDest) > entryGetLastModified(entrySrc))
+                                {
+                                    delete = 1;
+                                    entryFree(tempEntryDest);
+                                    stringFree(tempName);
+                                    stringFree(subName);
+                                    break;
+                                }
+                                else
+                                {
+                                    delete = 0;
+                                    remove(entryGetPath(tempEntryDest));
+                                }
+                            }
+
+                            entryFree(tempEntryDest);
+                            stringFree(tempName);
+                            stringFree(subName);
+                        }
+
+                        if(delete == 2 || delete == 0){
+                            int st3 = splitFile(entrySrc, tempDirDest);
+
+                            if(st3 == 1){
+                                printf("[ERROR] : Function splitFile failed | testBuild \n");
+                                return;
+                            }
+                        }
                     }
-                    else
+                    else if (entryDest == NULL || entryGetLastModified(entryDest) < entryGetLastModified(entrySrc))
                     {
                         char dest[MAX_LENGTH_PATH];
                         strcpy(dest, directoryGetPath(tempDirDest));
-                        strcat(dest, entryGetName(entry));
+                        strcat(dest, entryGetName(entrySrc));
 
-                        bool st2 = fileCopy(dest, entryGetPath(entry));
+                        bool st2 = fileCopy(dest, entryGetPath(entrySrc));
 
-                        if(st2 == false){
+                        if (st2 == false)
+                        {
                             printf("[ERROR] : function fileCopy failed | testBuild \n");
                             return;
                         }
@@ -983,9 +1027,10 @@ void testBuild(char *path)
 
                     stringFree(extension);
                     stringFree(name);
+                    entryFree(entryDest);
                 }
 
-                entryFree(entry);
+                entryFree(entrySrc);
             }
             directoryFree(tempDirSrc);
             directoryFree(tempDirDest);
--- src/main/c/Test/test.c
+++ src/main/c/Test/test.c
@@ -1,1 +1,206 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
 #include "test.h"
+#include "../Util/String/string.h"
+#include "../Util/List/list.h"
+
+enum TYPE
+{
+    MACRO,
+    FUNCTION,
+    COMMENT,
+    MULTI_COMMENT,
+    OTHER
+};
+
+/**
+ * Function used to update the splited files by adding additional information
+ * 
+ * @param splitFiles the list with the files
+ * @param token the addition
+ * 
+ * @return Success: 0 | Failure: -1
+ */
+int updateFiles(List *splitFiles, String *token);
+
+int splitFile(Entry *src, Directory *dest)
+{
+    if (src == NULL)
+    {
+        printf("[ERROR] : the src entry can not be null | splitFile \n");
+        return -1;
+    }
+
+    if (dest == NULL)
+    {
+        printf("[ERROR] : the dest directory can not be null | splitFile \n");
+        return -1;
+    }
+
+    FILE *srcFile = fopen(entryGetPath(src), "r");
+
+    if (srcFile == NULL)
+    {
+        printf("[ERROR] : src file could not be opened | splitFile \n");
+        return -1;
+    }
+
+    String *preSet = stringCreate(NULL);
+    String *token = stringCreate(NULL);
+
+    List *splitFiles = listCreate(stringSize(), &stringCopy, &stringFree);
+
+    int type;
+    int funcCounter;
+    int c;
+
+    while ((c = getc(srcFile)) != EOF)
+    {
+        if (stringLength(token) == 0 && isspace(c) == false)
+        {
+            if (c == '#')
+            {
+                type = MACRO;
+            }
+            else if (c == '/')
+            {
+                stringAdd(token, c);
+                c = getc(srcFile);
+
+                if (c == '/')
+                {
+                    type = COMMENT;
+                }
+                else if (c == '*')
+                {
+                    type = MULTI_COMMENT;
+                }
+            }
+            else
+            {
+                type = OTHER;
+            }
+            stringAdd(token, c);
+        }
+        else if (stringLength(token) == 0 && isspace(c))
+        {
+            stringAdd(preSet, c);
+        }
+        else if (type == MACRO)
+        {
+            stringAdd(token, c);
+
+            if (c == '\n')
+            {
+                updateFiles(splitFiles, token);
+                stringCat(preSet, token);
+                stringClear(token);
+            }
+        }
+        else if (type == COMMENT)
+        {
+            stringAdd(token, c);
+
+            if (c == '\n')
+            {
+                updateFiles(splitFiles, token);
+                stringCat(preSet, token);
+                stringClear(token);
+            }
+        }
+        else if (type == MULTI_COMMENT)
+        {
+            stringAdd(token, c);
+
+            if (c == '*')
+            {
+                c = getc(srcFile);
+
+                if (c == '/')
+                {
+                    stringAdd(token, c);
+                    updateFiles(splitFiles, token);
+                    stringCat(preSet, token);
+                    stringClear(token);
+                }
+                stringAdd(token, c);
+            }
+        }
+        else if (type == OTHER)
+        {
+            stringAdd(token, c);
+
+            if (c == ';')
+            {
+                updateFiles(splitFiles, token);
+                stringCat(preSet, token);
+                stringClear(token);
+            }
+            else if (c == '{')
+            {
+                type = FUNCTION;
+                funcCounter = 0;
+            }
+        }
+        else if (type == FUNCTION)
+        {
+            stringAdd(token, c);
+
+            if (c == '{')
+            {
+                funcCounter++;
+            }
+            else if (c == '}')
+            {
+                funcCounter--;
+
+                if (funcCounter < 0)
+                {
+                    String *tempFile = stringCreate(stringToArr(preSet));
+                    stringCat(tempFile, token);
+
+                    stringClear(token);
+
+                    listAdd(splitFiles, tempFile);
+
+                    stringFree(tempFile);
+                }
+            }
+        }
+    }
+    fclose(srcFile);
+
+    stringFree(preSet);
+    stringFree(token);
+
+    for(int i = 0; i < listLength(splitFiles); i++){
+        FILE *tempFile = fopen()
+    }
+}
+
+int updateFiles(List *splitFiles, String *token)
+{
+    if(splitFiles == NULL){
+        printf("[ERROR] : list can not be null | updateFiles \n");
+        return -1;
+    }
+
+    if(token == NULL){
+        printf("[ERROR] : string can not be null | updateFiles \n");
+        return -1;
+    }
+
+    for(int i = 0; i < listLength(splitFiles); i++){
+        String *temp = listGet(splitFiles, i);
+
+        stringCat(temp, token);
+
+        listSet(splitFiles, temp, i);
+
+        stringFree(temp);
+    }
+    return 0;
+}
